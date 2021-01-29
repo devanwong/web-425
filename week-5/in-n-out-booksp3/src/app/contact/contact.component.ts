cebf9fc45dc475380c9adf2e20d1a4153b777e32
Devan Wong <devan.wong@yahoo.com>
2021-01-28 19:59:33 -0800

    week5 5.4

---
+++ week-5/in-n-out-booksp3/src/app/contact/contact.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: contact.component.ts
+ * Author: Devan Wong
+ * Date: 27 January 2021
+ * Description: Contact file
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
